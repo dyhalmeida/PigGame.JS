76dde19cc9b5bde7025f15f27a05aa6682ad3bc5
Diego Almeida <dyhalmeida@gmail.com>
2018-03-16 09:58:31 -0300

    Lógica do score e troca de jogador se dado for 1

--- app.js
+++ app.js
@@ -1,37 +1,71 @@
 /*
-GAME RULES:
-
-- The game has 2 players, playing in rounds
-- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
-- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-- The first player to reach 100 points on GLOBAL score wins the game
-
 REGRAS DO JOGO:
 
 - O jogo tem 2 jogadores, jogando em rodadas.
-- Em cada turno, um jogador rola um dado quantas vezes ele deseja. Cada resultado é adicionado ao seu resultado ATUAL
-- MAS, se o jogador rolar um dado 1, todo o seu resultado ATUAL se perde e depois disso, é a vez do próximo jogador.
+- Em cada turno, um jogador joga um dado quantas vezes ele deseja. Cada resultado é adicionado ao seu resultado ATUAL. MAS, se o jogador jogar um dado 1, todo o seu resultado ATUAL se perde e depois disso é a vez do próximo jogador.
 - O jogador pode escolher 'Guardar', o que significa que seu resultado ATUAL é adicionado ao seu score GLOBAL e depois disso, é a vez do próximo jogador.
 - O primeiro jogador a alcançar 100 pontos no resultado GLOBAL ganha o jogo.
 
 */
 
- var scores, roundScore, activeplayer, dice;
+ var scores, roundScore, activeplayer;
 
 scores = [0,0]; // index 0 para jogador 1; index 1 para jogador 2
 roundScore = 0;
 activeplayer = 0; // Para jogador 1 o valor é 0; Para jogador 2 o valor é 1
 
-// dice/dado recebe um número aleatório de 1 a 6
-dice = Math.floor(Math.random() * 6) + 1;
-//console.log(dice);
-
-// Acessando o DOM e selecionado um elemento pelo ID
-// textContent não interpreta HTML para isso utiliza-se innerHTML
-document.querySelector('#current-' + activeplayer).textContent = dice; 
-//document.querySelector('#current-' + activeplayer).innerHTML = '<em>' + dice + '</em>'; 
-
-// Acessado o DOM por ID e manipulando propriedade CSS
-// Escondendo a imagem do DADO
+// Esconde a imagem do dado
 document.querySelector('.dice').style.display = 'none';
+
+// Zera o scores global e score parcial de ambos jogadores
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+// Adiciona uma escuta de evento ao botão de jogar o dado com função anônima de callback
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. Pega os números aleatório de 1 a 6
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Apresenta a imagem do dado e troca de acordo com o número aleatório
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.src = 'dice-' + dice + '.png';
+    diceDOM.style.display = 'block';
+    //console.log(dice);
+    
+    // 3. Atualiza a pontuação ATUAL se o número do dado não for 1
+    if(!(dice === 1)) {
+        
+        roundScore += dice;
+        document.querySelector('#current-' + activeplayer).textContent = roundScore;
+        
+    } else {
+        
+        // Altera o jogador da vez
+        activeplayer === 0 ? activeplayer = 1 : activeplayer = 0;
+        
+        // Zera o score atual se o dado for 1
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        
+        // Identifica qual o jogador da vez
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+        
+        // Escode o dado a cada vez que trocar de jogador
+        diceDOM.style.display = 'none';
+        
+    }
+    
+});
+
+
+
+
+
+
+
+
